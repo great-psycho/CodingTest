63976dfed51354875e4e0267d3791af191a2b3e7
유제협 <90807141+great-psycho@users.noreply.github.com>
2023-12-15 16:52:13 +0900

    [Bronze V] Title: A+B, Time: 216 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }    
+}
